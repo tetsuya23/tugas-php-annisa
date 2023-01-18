fa469479c2c8393116b2e40a681c6135093adec3
Annisareren <arman.annisaasthari10@gmail.com>
2023-01-19 03:37:16 +0700

    php

--- CRUD/add.php
+++ CRUD/add.php
@@ -11,12 +11,61 @@
     <a href="index.php">Go to Home</a>
     <br/><br/>
 
-    <form action="add.php" method="post" name="form">
+    <form action="add.php" method="post" name="form1">
         <table width="25%" border="0">
             <tr>
                 <td>nm_pembeli</td>
                 <td><input type="text" name="nm_pembeli"></td>
             </tr>
+            <tr>
+                <td>almt</td>
+                <td><input type="text" name="almt"></td>
+            </tr>
+            <tr>
+                <td>no_hp</td>
+                <td><input type="text" name="no_hp"></td>
+            </tr>
+            <tr>
+            <td>obat</td>
+				<td>
+					<select name="id">
+						<?php 
+						    while($obat_data = mysqli_fetch_array($obat)) {         
+						    	echo "<option value='".$obat_data['id']."'>".$obat_data['nm_obat']."</option>";
+						    }
+						?>
+					</select>
+				</td>
+			</tr>
+            <tr> 
+				<td>Transaksi</td>
+				<td>
+					<select name="id">
+						<?php 
+						    while($transaksi_data = mysqli_fetch_array($transaksi)) {         
+						    	echo "<option value='".$transaksi_data['id']."'>".$transaksi_data['status']."</option>";
+						    }
+						?>
+					</select>
+				</td>
+			</tr>
         </table>
+    </form>
+    <?php
+    // Check If form submitted, insert form data into users table.
+		if(isset($_POST['Submit'])) {
+			$nm_pembeli = $_POST['nm_pembeli'];
+			$almt = $_POST['almt'];
+			$no_hp = $_POST['no_hp'];
+			$id = $_POST['idobat'];
+			$id = $_POST['idtransaksi'];
+			
+            include_once("koneksi.php");
+
+			$result = mysqli_query($mysqli, "INSERT INTO `pembeli` (`nm_pembeli`, `almt`, `no_hp`, `id`, `id`, `id_katalog`) VALUES ('$nm_pembeli', '$almt', '$no_hp', '$id', '$id');");
+			
+			header("Location:index.php");
+        }
+    ?>
 </body>
 </html>
--- CRUD/index.php
+++ CRUD/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once("koneksi.php");
-$pembeli= mysqli_query($mysqli, "SELECT pembeli.*, nm_pembeli, idobat FROM 'pembeli'
-                         JOIN obat ON pembeli.idobat = detail.id ORDER BY id_detail ASC");
+$pembeli= mysqli_query($mysqli, "SELECT pembeli.*, nm_pembeli, idobat FROM pembeli
+                         JOIN obat ON pembeli.idobat = obat.id ORDER BY id_obat ASC");
 
 ?>
 
@@ -12,17 +12,17 @@ $pembeli= mysqli_query($mysqli, "SELECT pembeli.*, nm_pembeli, idobat FROM 'pemb
     <body>
         <center>
             <a href="index.php">Obat</a> |
-            <a href=" ">Pembeli</a> |
-            <a href="">Dokter</a> |
-            <a href="">Petugas</a> |
-            <a href="">Detail</a> |
-            <a href="">Transaksi</a>
+            <a href="#">Pembeli</a> |
+            <a href="#">Dokter</a> |
+            <a href="#">Petugas</a> |
+            <a href="#">Detail</a> |
+            <a href="#">Transaksi</a>
             <hr>
         </center>
 
         <a href="add.php">Add Menu Pembeli</a><hr/></hr>
 
-            <table width='80%' border=1>
+            <table class="table" width='80%' border=1>
                 <tr>
                     <th>nm_pembeli</th>
                     <th>almt</th>
